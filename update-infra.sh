a9aa463ae84ee8d2fc795ff8a2c82454bfc57bea
Rodrigo Justino da Costa <rodrigo.costa@sensedia.com>
2019-05-28 16:51:43 -0300

    Troca o comando de build

--- update-infra.sh
+++ update-infra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose up -d --no-deps --build rabbitmq mysql elasticsearch kibana logstash prometheus node-exporter alertmanager grafana blackbox cadvisor
+docker-compose build --no-cache
