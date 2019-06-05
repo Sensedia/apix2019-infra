8cc8b06056396120a4ee3e342a1086dc89e6b7ad
Rodrigo Justino da Costa <rodrigo.costa@sensedia.com>
2019-06-05 16:59:27 -0300

    Altera a regra do sg

--- aws-eks/cluster/lb.tf
+++ aws-eks/cluster/lb.tf
@@ -1,21 +1,20 @@
 resource "aws_elb" "elb_gtw" {
   name = "${var.cluster_name}"
-  #security_groups = "${aws_security_group.kubernetes-lb}"
+  security_groups = ["${aws_security_group.kubernetes-node.id}"]
   subnets = ["${aws_subnet.demo.*.id}"]
 
   listener {
-    instance_port = 8080
-    instance_protocol = "http"
+    instance_port = 30001
+    instance_protocol = "TCP"
     lb_port = 80
-    lb_protocol = "http"
+    lb_protocol = "TCP"
   }
 
   health_check {
     healthy_threshold = 3
     unhealthy_threshold = 2
     timeout = 2
-    target = "HTTP:8080/gateway-admin/enabled"
-    interval = 10
+    target = "TCP:30001"
+    interval = 5
   }
 }
-
